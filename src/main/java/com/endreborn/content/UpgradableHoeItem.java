a9f3724e2a8d07f3bc2decd70d2240ef5af35518
Immodernist <140558252+Immodernist@users.noreply.github.com>
2024-03-01 23:46:14 +0200

    1.20.4

--- src/main/java/com/endreborn/content/UpgradableHoeItem.java
+++ src/main/java/com/endreborn/content/UpgradableHoeItem.java
@@ -1,21 +1,28 @@
 package com.endreborn.content;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.HoeItem;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Tier;
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class UpgradableHoeItem extends HoeItem {
     private final int sharpness;
     private final int flexibility;
@@ -31,8 +38,14 @@ public class UpgradableHoeItem extends HoeItem {
 
     @Override
     public float getDestroySpeed(ItemStack stack, BlockState state) {
-        return state.is(BlockTags.MINEABLE_WITH_HOE) ? this.speed + this.sharpness * 2.4F: 1.0F;
+        if (sharpness == 1){
+            return (state.is(BlockTags.MINEABLE_WITH_HOE) || state.is(BlockTags.ICE)) ? this.speed: 1.0F;
+        }
+        else {
+            return (state.is(BlockTags.MINEABLE_WITH_HOE)) ? this.speed: 1.0F;
+        }
     }
+
     @OnlyIn(Dist.CLIENT)
     public void appendHoverText(ItemStack stack, @Nullable Level world, List<Component> tooltip, TooltipFlag flag) {
         tooltip.add(Component.translatable("tooltip.relic").withStyle(ChatFormatting.GRAY));
